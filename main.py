0c407c7bfd323edb4a497a3c3895ff0f5425ecc5
MHE2006 <merijnstreefkerk@gmail.com>
2025-09-05 12:04:09 +0200

    second try

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def add_two_integer_lists(a: list[int], b: list[int]) -> str:
+    """
+    Function adding two integer lists together 
+    """
     arr = np.array(a) + np.array(b)
-    print(f"My added array: {arr}")
+    return arr
 
 
 if __name__ == "__main__":
-    add_two_integer_lists([1, 2, 3], [4, 5, 6])
+    print(f"My added array:", add_two_integer_lists([1, 2, 3], [4, 5, 6]))

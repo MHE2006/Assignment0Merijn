7158be1400d6bcbbe858679c1318c97d4a0303aa
MHE2006 <merijnstreefkerk@gmail.com>
2025-09-05 11:56:52 +0200

    Complete project setup and add numpy script

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def add_two_integer_lists(a: list[int], b: list[int]) -> str:
+    arr = np.array(a) + np.array(b)
+    print(f"My added array: {arr}")
+
+
+if __name__ == "__main__":
+    add_two_integer_lists([1, 2, 3], [4, 5, 6])
